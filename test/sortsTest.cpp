7894d3b77010f3ff4af9568ca55feb4a2fa1e9a3
Movest <forentop@yandex.ru>
2020-11-02 16:12:56 +0700

    Added tests for other sorts

--- test/sortsTest.cpp
+++ test/sortsTest.cpp
@@ -14,7 +14,7 @@ TEST_CASE("QuickSort", "[test]")
         sorted_array = {"bad", "123", "OK", "4tr1"};
         WHEN("Sorting")
         {
-            QuickSort(&sorted_array, 0, sorted_array.size());
+            QuickSort(&sorted_array, 0, sorted_array.size() - 1);
             THEN("Compare")
             {
                 std::string expected = "123";
@@ -28,7 +28,7 @@ TEST_CASE("QuickSort", "[test]")
         sorted_array = {"bad", "ok3214", "OK", "4tr1"};
         WHEN("Sorting")
         {
-            QuickSort(&sorted_array, 0, sorted_array.size());
+            QuickSort(&sorted_array, 0, sorted_array.size() - 1);
             THEN("Compare")
             {
                 std::string expected = "4tr1";
@@ -36,4 +36,77 @@ TEST_CASE("QuickSort", "[test]")
             }
         }
     }
+    array.clear();
+    sorted_array.clear();
+}
+TEST_CASE("ShellSort", "[test]")
+{
+    std::vector<std::string> array;
+    std::vector<std::string> sorted_array;
+    GIVEN("VECTOR TO SORT")
+    {
+        array = {"bad", "123", "OK", "4tr1"};
+        sorted_array = {"bad", "123", "OK", "4tr1"};
+        WHEN("Sorting")
+        {
+            ShellSort(&sorted_array, sorted_array.size());
+            THEN("Compare")
+            {
+                std::string expected = "123";
+                REQUIRE(expected == sorted_array[0]);
+            }
+        }
+    }
+    GIVEN("VECTOR TO SORT")
+    {
+        array = {"bad", "ok3214", "OK", "4tr1"};
+        sorted_array = {"bad", "ok3214", "OK", "4tr1"};
+        WHEN("Sorting")
+        {
+            ShellSort(&sorted_array, sorted_array.size());
+            THEN("Compare")
+            {
+                std::string expected = "4tr1";
+                REQUIRE(expected == sorted_array[0]);
+            }
+        }
+    }
+    array.clear();
+    sorted_array.clear();
+}
+
+TEST_CASE("BubbleSort", "[test]")
+{
+    std::vector<std::string> array;
+    std::vector<std::string> sorted_array;
+    GIVEN("VECTOR TO SORT")
+    {
+        array = {"bad", "123", "OK", "4tr1"};
+        sorted_array = {"bad", "123", "OK", "4tr1"};
+        WHEN("Sorting")
+        {
+            BubbleSort(&sorted_array, sorted_array.size());
+            THEN("Compare")
+            {
+                std::string expected = "123";
+                REQUIRE(expected == sorted_array[0]);
+            }
+        }
+    }
+    GIVEN("VECTOR TO SORT")
+    {
+        array = {"bad", "ok3214", "OK", "4tr1"};
+        sorted_array = {"bad", "ok3214", "OK", "4tr1"};
+        WHEN("Sorting")
+        {
+            BubbleSort(&sorted_array, sorted_array.size());
+            THEN("Compare")
+            {
+                std::string expected = "4tr1";
+                REQUIRE(expected == sorted_array[0]);
+            }
+        }
+    }
+    array.clear();
+    sorted_array.clear();
 }
